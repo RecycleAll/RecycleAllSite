8eda6da3170da742820300d595596a5609eb599d
nathanaelto <nathanael.to@free.fr>
2021-07-18 01:32:13 +0200

    Configuration final + début de navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AuthUserService} from "./services/auth-user.service";
+import { NavComponent } from './components/nav/nav.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { ErrorAccessComponent } from './components/error-access/error-access.component';
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: '', component: HomeComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'not-found', component: ErrorAccessComponent},
+  { path: '**', redirectTo: 'not-found'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SignupComponent,
+    LoginComponent,
+    ErrorAccessComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    AuthUserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/error-access/error-access.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-access',
+  templateUrl: './error-access.component.html',
+  styleUrls: ['./error-access.component.scss']
+})
+export class ErrorAccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
