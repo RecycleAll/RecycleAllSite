e5805ea6dadad17ad2f7237e63482c1298f0e102
nathanaelto <nathanael.to@free.fr>
2021-07-21 02:08:37 +0200

    add btn delete user

--- src/app/components/admin/user-list/single-user/single-user.component.ts
+++ src/app/components/admin/user-list/single-user/single-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, ActivatedRouteSnapshot, Router} from "@angular/router";
+import {UserService} from "../../../../services/user.service";
+import {User} from "../../../../models/user.model";
 
 @Component({
   selector: 'app-single-user',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleUserComponent implements OnInit {
 
-  constructor() { }
+  user !: User;
 
-  ngOnInit(): void {
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private userService: UserService
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    const id = this.route.snapshot.params['id'];
+    await this.initUser(id);
   }
+
+  async initUser(id: number){
+    this.user = await this.userService.getOne(id);
+  }
+
+  async onDelete() {
+    const isDelete = await this.userService.delete(this.user.id);
+    if(isDelete){
+      this.router.navigate(['/admin/user'])
+    }else{
+      alert("Is not delete !");
+    }
+  }
+
 
 }
