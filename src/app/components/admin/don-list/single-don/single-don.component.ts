2eb06f3d0d27fa402ea57e8948fe60c1386dbe49
Clement <clementhottotpro@gmail.com>
2021-07-24 16:47:38 +0200

    fix link don / product

--- src/app/components/admin/don-list/single-don/single-don.component.ts
+++ src/app/components/admin/don-list/single-don/single-don.component.ts
@@ -4,6 +4,8 @@ import {Don} from "../../../../models/don.model";
 import {DonService} from "../../../../services/don.service";
 import {User} from "../../../../models/user.model";
 import {UserService} from "../../../../services/user.service";
+import {Product} from "../../../../models/product.model";
+import {ProductsService} from "../../../../services/products.service";
 
 @Component({
   selector: 'app-single-don',
@@ -13,12 +15,14 @@ import {UserService} from "../../../../services/user.service";
 export class SingleDonComponent implements OnInit {
 
   user?: User;
+  products: Product[] = [];
 
   @Input() don!: Don;
   constructor(private router: Router,
               private route: ActivatedRoute,
               private donService: DonService,
-              private userService: UserService) {
+              private userService: UserService,
+              private productService: ProductsService) {
   }
 
   async ngOnInit() {
@@ -27,6 +31,9 @@ export class SingleDonComponent implements OnInit {
 
     if (this.don.user_id)
       this.user = await this.userService.getOne(this.don.user_id);
+
+
+    this.products = await this.productService.getAllByDon(this.don.id);
   }
 
   async initDon(id: number) {
