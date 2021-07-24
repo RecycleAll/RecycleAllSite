8acd9934d38f73d753d0ea6f872788d275ee1238
nathanaelto <nathanael.to@free.fr>
2021-07-24 14:46:23 +0200

    add new media (not finish)

--- src/app/components/admin/media-list/new-media/new-media.component.ts
+++ src/app/components/admin/media-list/new-media/new-media.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MediaType} from "../../../../models/media-type.model";
+import {Subscription} from "rxjs";
+import {MediaTypeService} from "../../../../services/media-type.service";
+import {MediaService} from "../../../../services/media.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-new-media',
@@ -8,10 +14,44 @@ import { Component, OnInit } from '@angular/core';
 export class NewMediaComponent implements OnInit {
 
   // TODO
+  mediaForm !: FormGroup;
 
-  constructor() { }
+  mediaTypes: MediaType[] = [];
+  mediaTypesSubscription!: Subscription;
 
-  ngOnInit(): void {
+  constructor(private router: Router,
+              private formBuilder: FormBuilder,
+              private mediaTypeService: MediaTypeService,
+              private mediaService: MediaService) { }
+
+  async ngOnInit(): Promise<void> {
+    await this.mediaTypesFetch();
+    this.mediaTypesSubscription = this.mediaTypeService.mediaTypeSubject.subscribe(
+      (mediaTypes: MediaType[]) => {
+        this.mediaTypes = mediaTypes;
+      }
+    );
+    this.mediaTypeService.emitMediaType();
+
+    this.initForm();
+  }
+
+  initForm() {
+    this.mediaForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      client_view: ['', Validators.required],
+      media_type_id: ['', Validators.required],
+      file: []
+    });
+  }
+
+  async mediaTypesFetch() {
+    await this.mediaTypeService.getAll();
+  }
+
+  async onSubmitForm() {
+    console.log(this.mediaForm.value);
+    console.log("file : ", this.mediaForm.value.file);
   }
 
 }
--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -45,7 +45,7 @@ export class MediaService {
       environment.API_URL + 'media/',
       {
         name: props.name,
-        path: props.name,
+        path: props.path,
         client_view: props.client_view,
         media_type_id: props.media_type_id,
         user_save: props.user_save
@@ -56,6 +56,25 @@ export class MediaService {
     })).toPromise();
 
     if (promise.status === 201 && promise.body !== null) {
+      return promise.body;
+    }
+    return null;
+  }
+
+  async uploadFile(file: string, id: number){
+    const formData: FormData = new FormData();
+    formData.append("uploaded_file", file);
+    formData.append("id", id.toString())
+
+    const promise = await this.httpClient.post<Media>(
+      environment.API_URL + 'media/file',
+      formData,
+      {observe: 'response'}
+    ).pipe(catchError(() => {
+      return EMPTY;
+    })).toPromise();
+
+    if (promise.status === 200 && promise.body !== null){
       return promise.body;
     }
     return null;
