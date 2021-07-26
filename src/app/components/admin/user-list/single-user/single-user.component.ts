bec4b8d150e8c02b52c1a3816e0277d80311baaa
Clement <clementhottotpro@gmail.com>
2021-07-26 16:56:10 +0200

    added user edition/view  address link

--- src/app/components/admin/user-list/single-user/single-user.component.ts
+++ src/app/components/admin/user-list/single-user/single-user.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, ActivatedRouteSnapshot, Router} from "@angular/router";
 import {UserService} from "../../../../services/user.service";
 import {User} from "../../../../models/user.model";
+import {AddressService} from "../../../../services/address.service";
+import {UserAddressService} from "../../../../services/user-address.service";
+import {Address} from "../../../../models/address.model";
+import {Entrepot} from "../../../../models/entrepot.model";
 
 @Component({
   selector: 'app-single-user',
@@ -11,26 +15,40 @@ import {User} from "../../../../models/user.model";
 export class SingleUserComponent implements OnInit {
 
   user !: User;
+  entrepots: Entrepot[] = [];
+  address: Address[] = [];
 
   constructor(private router: Router,
               private route: ActivatedRoute,
-              private userService: UserService
-  ) { }
+              private userService: UserService,
+              private addressService: AddressService,
+              private userAddressService: UserAddressService) {}
 
   async ngOnInit(): Promise<void> {
     const id = this.route.snapshot.params['id'];
     await this.initUser(id);
   }
 
-  async initUser(id: number){
+  async initUser(id: number) {
     this.user = await this.userService.getOne(id);
+
+    let userAddresses = await this.userAddressService.getAllByUser(this.user.id);
+    for (let ua of userAddresses) {
+      if(ua.address_id) {
+        let a = (await this.addressService.getOne(ua.address_id))
+        if (a)
+          this.address.push(a);
+      }
+    }
+    console.log(userAddresses);
+    console.log(this.address);
   }
 
   async onDelete() {
     const isDelete = await this.userService.delete(this.user.id);
-    if(isDelete){
+    if (isDelete) {
       this.router.navigate(['/admin/user'])
-    }else{
+    } else {
       alert("Is not delete !");
     }
   }
--- src/app/components/admin/user-list/user-update/user-update.component.ts
+++ src/app/components/admin/user-list/user-update/user-update.component.ts
@@ -7,6 +7,14 @@ import {AuthUserService} from "../../../../services/auth-user.service";
 import {EntrepotService} from "../../../../services/entrepot.service";
 import {User} from "../../../../models/user.model";
 import {UserService} from "../../../../services/user.service";
+import {Address} from "../../../../models/address.model";
+import {AddressService} from "../../../../services/address.service";
+import {UserAddressService} from "../../../../services/user-address.service";
+
+interface AddressItem {
+  userAddressId?: number,
+  address: Address
+}
 
 @Component({
   selector: 'app-user-update',
@@ -20,18 +28,24 @@ export class UserUpdateComponent implements OnInit {
   entrepotsSubscription!: Subscription;
   user!: User;
 
+  allAddress: AddressItem[] = [];
+  currentAddress?: AddressItem;
+
+  userAddress: AddressItem[] = [];
+  addedAddress: AddressItem[] = [];
+  removedAddress: AddressItem[] = [];
+
   constructor(private formBuilder: FormBuilder,
               private authUser: AuthUserService,
               private router: Router,
               private entrepotService: EntrepotService,
               private route: ActivatedRoute,
-              private userService: UserService) {
+              private userService: UserService,
+              private addressService: AddressService,
+              private userAddressService: UserAddressService) {
   }
 
   async ngOnInit(): Promise<void> {
-    const id = this.route.snapshot.params['id'];
-    await this.initUser(id);
-    this.initForm();
     await this.entrepotsFetch()
     this.entrepotsSubscription = this.entrepotService.entrepotSubject.subscribe(
       (entrepots: Entrepot[]) => {
@@ -39,10 +53,36 @@ export class UserUpdateComponent implements OnInit {
       }
     )
     this.entrepotService.emitEntrepot();
+
+    await this.addressFetch()
+    this.addressService.addressSubject.subscribe(
+      (address: Address[]) => {
+        this.allAddress = address.map(value => {
+          return {
+            address: value,
+          }
+        });
+      }
+    )
+    this.addressService.emitAddress();
+
+    const id = this.route.snapshot.params['id'];
+    await this.initUser(id);
+    this.initForm();
   }
 
   async initUser(id: number) {
     this.user = await this.userService.getOne(id);
+
+    let userAddresses = await this.userAddressService.getAllByUser(this.user.id);
+    for (let ua of userAddresses) {
+      let i = this.allAddress.findIndex(value => value.address.id === ua.address_id);
+      if(i >= 0){
+        this.allAddress[i].userAddressId = ua.id;
+        this.userAddress.push( this.allAddress[i]);
+        this.allAddress.splice(i, 1);
+      }
+    }
   }
 
   initForm() {
@@ -58,6 +98,53 @@ export class UserUpdateComponent implements OnInit {
   async entrepotsFetch() {
     await this.entrepotService.getAll();
   }
+
+  async addressFetch() {
+    await this.addressService.getAll();
+  }
+
+  addAddress() {
+    if (this.currentAddress) {
+
+      let i = this.allAddress.indexOf(this.currentAddress);
+      if (i >= 0) {
+        this.addedAddress.push(this.currentAddress);
+        this.allAddress.splice(i, 1);
+      }else{
+
+        i = this.removedAddress.indexOf(this.currentAddress);
+        if (i >= 0) {
+          this.userAddress.push(this.currentAddress);
+          this.removedAddress.splice(i, 1);
+        }
+      }
+    }
+  }
+
+  setSelectAddress(address_id: string) {
+    const id = Number(address_id);
+    this.currentAddress = this.allAddress.find(value => value.address.id === id);
+    if(!this.currentAddress){
+      this.currentAddress = this.removedAddress.find(value => value.address.id === id);
+    }
+  }
+
+  removeAddress(address: AddressItem) {
+    let i = this.addedAddress.indexOf(address);
+    if (i >= 0) {
+      this.allAddress.push(address);
+      this.addedAddress.splice(i, 1);
+    }
+  }
+
+  removeAddress2(address: AddressItem) {
+    let i = this.userAddress.indexOf(address);
+    if (i >= 0) {
+      this.removedAddress.push(address);
+      this.userAddress.splice(i, 1);
+    }
+  }
+
 
   async onSubmitForm() {
     let {firstname, lastname, email, newpassword, entrepots} = this.userForm.value;
@@ -87,7 +174,24 @@ export class UserUpdateComponent implements OnInit {
         password: newpassword,
         work_in: entrepots
       });
+
       if (res !== null) {
+
+        for(let a of this.addedAddress){
+          if(!a.userAddressId){
+            await this.userAddressService.create({
+              user_id: this.user.id,
+              address_id: a.address.id
+            })
+          }
+        }
+
+        for(let a of this.removedAddress){
+          if(a.userAddressId){
+            await this.userAddressService.delete(a.userAddressId);
+          }
+        }
+
         this.router.navigate(['/admin/user']);
       } else {
         alert("Error of update");
